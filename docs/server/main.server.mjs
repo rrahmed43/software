8f73cdbdfc8182fac7cc3cfaafdaa2f4e3232c30
rawan ahmed <rrahmeed43@yahoo.com>
2025-03-16 21:57:56 +0200

    Deploy Angular to GitHub Pages

---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-NNFIZGUK.mjs";import"./chunk-M44VJ6MS.mjs";import"./chunk-CSPUXJSW.mjs";import"./chunk-OIPSUF4K.mjs";import"./chunk-3CVZQN5Z.mjs";export{a as default};
